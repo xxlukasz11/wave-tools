715ae360d4c7d1d182c2a77202838c8e0885a342
Łukasz Bożek <xxlukasz11@gmail.com>
2021-01-02 19:54:25 +0100

    fix subchunks size reporting

--- subChunk.cpp
+++ subChunk.cpp
@@ -1,10 +1,12 @@
 #include "subChunk.h"
 
+constexpr int SUBCHUNK_BASIC_SIZE = 8;
+
 SubChunk::SubChunk(const ChunkPtr& masterChunk, const uint32_t subChunkId, const uint32_t initialSubChunkSize) :
 	mMasterChunk(masterChunk),
 	mSubChunkId(subChunkId),
 	mSubChunkSize(initialSubChunkSize) {
-	mMasterChunk->increaseChunkSize(mSubChunkSize);
+	mMasterChunk->increaseChunkSize(SUBCHUNK_BASIC_SIZE + mSubChunkSize);
 }
 
 uint32_t SubChunk::getSubChunkId() const {
